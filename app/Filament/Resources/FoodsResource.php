211f2acbeb7479c941dc5fdae23cf4d27d3a7079
RahulKen96 <rahulKen96@gmail.com>
2025-04-09 14:51:15 +0700

    [Update] Create filament resources

---
+++ app/Filament/Resources/FoodsResource.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FoodsResource\Pages;
+use App\Filament\Resources\FoodsResource\RelationManagers;
+use App\Models\Foods;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FoodsResource extends Resource
+{
+    protected static ?string $model = Foods::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('image')
+                    ->image()
+                    ->required(),
+                Forms\Components\TextInput::make('price')
+                    ->numeric()
+                    ->default(null)
+                    ->prefix('$'),
+                Forms\Components\TextInput::make('price_afterdiscount')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\TextInput::make('percent')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\Toggle::make('is_promo'),
+                Forms\Components\TextInput::make('categories_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price_afterdiscount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('percent')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_promo')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('categories_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFoods::route('/'),
+            'create' => Pages\CreateFoods::route('/create'),
+            'edit' => Pages\EditFoods::route('/{record}/edit'),
+        ];
+    }
+}
